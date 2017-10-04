9963a025a39b059952081d08d45a57f1443a35aa
Chris Clifford <chris.clifford@acceptto.com>
2017-10-03 18:29:27 -0700

    add styling and create/delete routes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,37 +1,51 @@
 class SessionController < ApplicationController
+  BaseURL = "http://localhost:3002"
   def index
     @token = nil
     if params[:code] && params[:state] == session[:state]
-      auth_request_uri = "http://localhost:3002/oauth/token?" +
+      auth_request_uri = "#{BaseURL}/oauth/token?" +
                         "client_id=584e229b9bfa46a2b79d055d907eb1d94f38c4a31b8d1f57aef32bb21091272c&" +
                         "client_secret=ef3055aa00eb757085b3d18894dd760dc56c927839a6a0a8f125c9c979f4f2cd&" +
                         "code=#{params[:code]}&redirect_uri=http://localhost:3000/&grant_type=authorization_code&scope=openid"
       request = HTTParty.post(auth_request_uri)
+
       if request['id_token']
-        split_token = request['id_token'].split('.')
-        @header = Base64.decode64(split_token[0])
-        @payload = Base64.decode64(split_token[1])
-        @signature = Base64.decode64(split_token[2])
+        session[:id_token] = request['id_token']
       end
-      
-      render :index
+      if session[:id_token]
+        split_token = session[:id_token].split('.')
+        @payload = JSON.parse(Base64.decode64(split_token[1]))
+      end
     elsif params[:code]
-      redirect_to root, alert: 'State value does not match'
+      redirect_to login_path, alert: 'State value does not match'
+    else
+      redirect_to login_path
     end
   end
 
   def new
     state = SecureRandom.hex(30)
     session[:state] = state
-    @code_request_uri = "http://localhost:3002/oauth/authorize?response_type=code&" +
+    @code_request_uri = "#{BaseURL}/oauth/authorize?response_type=code&" +
                         "client_id=584e229b9bfa46a2b79d055d907eb1d94f38c4a31b8d1f57aef32bb21091272c&" +
                         "client_secret=ef3055aa00eb757085b3d18894dd760dc56c927839a6a0a8f125c9c979f4f2cd&" +
                         "redirect_uri=http://localhost:3000/&scope=openid&state=#{state}"
   end
 
+  def create
+    token = {header: {}, payload: {'iss': 'www.acceptto.com', sub: params[:email], exp: Time.now.to_i, iat: Time.now.to_i}}
+    @payload = token[:payload].with_indifferent_access
+    render :index
+  end
+
+  def delete
+    reset_session
+    redirect_to root_path
+  end
+
   private
 
   def code_params
-    params.permit(:code, :state)
+    params.permit(:code, :state, :username, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/login', to: 'session#new'
-
+  get '/logout', to: 'session#delete'
+  post '/login', to: 'session#create'
 
   root 'session#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
